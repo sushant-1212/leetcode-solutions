085bd47d33458977fbafd09275b0b4ca45bc1d75
sushant-1212 <sushantferia11@gmail.com>
2025-08-24 10:43:49 +0000

    Sync LeetCode submission Runtime - 37 ms (23.20%), Memory - 45 MB (99.77%)

---
+++ 1284-four-divisors/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int n = 100000 + 1;
+        int countDivisors[] = new int[n];
+        int sumDivisors[] = new int[n];
+        for(int i = 1; i<n; i++){
+            for(int j = i; j<n; j+=i){
+                countDivisors[j]++;
+                sumDivisors[j]+=i;
+            }
+        }
+        int N = nums.length;
+        int count = 0;
+        for(int i = 0; i<N; i++){
+            if(countDivisors[nums[i]]==4){
+                count+=sumDivisors[nums[i]];
+            }
+        }
+        return count;
+    }
+}
