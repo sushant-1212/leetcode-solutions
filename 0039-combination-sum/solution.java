13b0114753ba3796f891dd403f9ae9fea1564bbf
sushant-1212 <sushantferia11@gmail.com>
2025-09-02 18:14:03 +0000

    Sync LeetCode submission Runtime - 1 ms (99.96%), Memory - 45 MB (99.42%)

---
+++ 0039-combination-sum/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    List<List<Integer>> answer = new ArrayList<>();
+    List<Integer> currentSeq = new ArrayList<>();
+    public void recursion(int target , int candidates[] , int start){
+        if(target == 0){
+            answer.add(new ArrayList<Integer>(currentSeq));
+            return;
+        }
+        for(int i = start; i<candidates.length; i++){
+            if(target>=candidates[i]){
+                currentSeq.add(candidates[i]);
+                recursion(target - candidates[i],candidates,i);
+                currentSeq.remove(currentSeq.size()-1);
+            }
+        }
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        recursion(target,candidates,0);
+        return answer;        
+    }
+}
