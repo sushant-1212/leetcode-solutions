e0c66408111de977571a8f2d3da70ae308e69c5f
sushant-1212 <sushantferia11@gmail.com>
2025-09-07 18:30:04 +0000

    Sync LeetCode submission Runtime - 5 ms (82.39%), Memory - 43.8 MB (99.99%)

---
+++ 0009-palindrome-number/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int ans=0;
+        int rev = 0;
+        int s = x;
+        if(x<0){
+            return false;
+        }
+        while(s>0){
+            int lastdigit = s%10;
+            rev = rev*10 + lastdigit;
+            s = s/10;
+            
+            }
+            return x==rev;
+        }
+        
+    }
+
