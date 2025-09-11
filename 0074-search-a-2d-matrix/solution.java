85a2b60b4ac075de3d07692951d1c365e9ca0572
sushant-1212 <sushantferia11@gmail.com>
2025-09-11 18:34:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.4 MB (99.98%)

---
+++ 0074-search-a-2d-matrix/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int low = 0, high = m * n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;  
+            int val = matrix[mid / n][mid % n];
+
+            if (val == target) return true;
+            else if (val < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return false;
+    }
+}
