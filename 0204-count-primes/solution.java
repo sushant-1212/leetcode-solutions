631ddde040f57569071e9d5829f2b7ff68df8bd6
sushant-1212 <sushantferia11@gmail.com>
2025-09-23 18:21:35 +0000

    Sync LeetCode submission Runtime - 108 ms (49.18%), Memory - 49 MB (98.75%)

--- 0204-count-primes/solution.java
+++ 0204-count-primes/solution.java
@@ -1,17 +1,17 @@
 class Solution{
     public int countPrimes(int n){
         boolean isPrime[] = new boolean[n];
-        for(int i = 2; i<n; i++){
+        int count = 0;
+        for(int i =2; i<n; i++){
             isPrime[i] = true;
         }
         for(int i = 1; i*i<n; i++){
             if(isPrime[i]==true){
                 for(int j = i*i; j<n; j+=i){
-                    isPrime[j]=false;
+                    isPrime[j] = false;
                 }
             }
         }
-        int count = 0;
         for(int i = 0; i<n; i++){
             if(isPrime[i]==true){
                 count++;
@@ -20,7 +20,6 @@ class Solution{
         return count;
     }
 }
- 
        
         
 
