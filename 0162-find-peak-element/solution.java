a09eec390ddf3f203b676a92586e3da4c949f5c3
sushant-1212 <sushantferia11@gmail.com>
2025-11-07 20:54:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44 MB (87.93%)

---
+++ 0162-find-peak-element/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        int st = 0;
+        int end = n-1;
+        while(st<end){
+            int mid = st+(end-st)/2;
+            if(nums[mid]<nums[mid+1]){
+                st = mid+1;
+            } else{
+                end = mid;
+            }
+        }
+        return st;
+    }
+}
