690ff72aceeb4a4b8e0c9ad23fd5e05748d21c7f
sushant-1212 <sushantferia11@gmail.com>
2025-11-15 17:38:55 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 46.3 MB (83.18%)

---
+++ 0445-add-two-numbers-ii/solution.java
@@ -0,0 +1,80 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+     public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode temp = head;
+        while(temp!=null){
+            ListNode next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+        return prev;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1 = reverseList(l1);
+        l2 = reverseList(l2);
+        ListNode i =l1;
+        ListNode j = l2;
+        int carry = 0;
+        ListNode ansH = null;
+        ListNode ansT = null;
+        while(i!=null && j!=null){
+            int sum = i.val + j.val + carry;
+            carry = sum/10;
+            ListNode newNode = new ListNode(sum%10);
+            if(ansH == null && ansT == null){
+                ansH = newNode;
+                ansT = newNode;
+            } else{
+                ansT.next = newNode;
+                ansT = newNode;
+            }
+            i=i.next;
+            j=j.next;
+        }
+        while(i!=null){
+            int sum = i.val + carry;
+            carry = sum/10;
+            ListNode newNode = new ListNode(sum%10);
+            if(ansH == null && ansT == null){
+                ansH = newNode;
+                ansT = newNode;
+            } else{
+                ansT.next = newNode;
+                ansT = newNode;
+            }
+            i=i.next;
+        }
+        while(j!=null){
+            int sum = j.val + carry;
+            carry = sum/10;
+            ListNode newNode = new ListNode(sum%10);
+            if(ansH == null && ansT == null){
+                ansH = newNode;
+                ansT = newNode;
+            } else{
+                ansT.next = newNode;
+                ansT = newNode;
+            }
+            j=j.next;
+        }
+        if(carry>0){
+            ListNode newNode = new ListNode(carry);
+            ansT.next = newNode;
+            ansT = newNode;
+        }
+        ansH = reverseList(ansH);
+        return ansH;
+    }
+}
