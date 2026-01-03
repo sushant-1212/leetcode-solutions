a11f10b41a01ce7be2165ce44a86a276a5c6fecb
sushant-1212 <sushantferia11@gmail.com>
2026-01-03 12:28:57 +0000

    Sync LeetCode submission Runtime - 1 ms (99.93%), Memory - 46.8 MB (64.81%)

--- 0136-single-number/solution.java
+++ 0136-single-number/solution.java
@@ -1,10 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-         int res = 0;
-          for(int i = 0; i<nums.length; i++){
-            res = res ^ nums[i];
-          }
-          return res;
-          
+        int ans = 0;
+        for(int i = 0; i<nums.length; i++){
+            ans = ans ^ nums[i];
+        }
+        return ans;
+        
     }
 }
