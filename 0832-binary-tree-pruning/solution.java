05790b96d0118f663d6ddcefab74fb8776b62713
sushant-1212 <sushantferia11@gmail.com>
2026-01-14 17:42:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.8 MB (82.50%)

--- 0832-binary-tree-pruning/solution.java
+++ 0832-binary-tree-pruning/solution.java
@@ -20,7 +20,7 @@ class Solution {
         }
         root.left = pruneTree(root.left);
         root.right = pruneTree(root.right);
-        if(root.val==0 && root.left==null && root.right == null){
+        if(root.val==0 && root.left==null && root.right==null){
             return null;
         }
         return root;
