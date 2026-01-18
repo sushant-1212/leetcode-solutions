d3bf1b29c0b71454d7bf27aeb0374db333ab0cd1
sushant-1212 <sushantferia11@gmail.com>
2026-01-18 19:12:52 +0000

    Sync LeetCode submission Runtime - 37 ms (96.15%), Memory - 71.8 MB (22.94%)

---
+++ 2016-reduction-operations-to-make-the-array-elements-equal/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reductionOperations(int[] nums) {
+        int ops = 0;
+        int steps = 0;
+        Arrays.sort(nums);
+        for(int i = 1; i<nums.length; i++){
+            if(nums[i]!=nums[i-1]){
+                steps++;
+            }
+            ops+=steps;
+        }
+        return ops;
+        
+    }
+}
