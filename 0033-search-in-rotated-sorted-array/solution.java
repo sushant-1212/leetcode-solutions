be71734c535c15ef84269df5c674a6455710d676
sushant-1212 <sushantferia11@gmail.com>
2026-01-21 18:45:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.8 MB (47.01%)

--- 0033-search-in-rotated-sorted-array/solution.java
+++ 0033-search-in-rotated-sorted-array/solution.java
@@ -8,13 +8,13 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             } else if(nums[st]<=nums[mid]){
-                if(target>=nums[st]&& target<=nums[mid]){
+                if(target>=nums[st]&& target<nums[mid]){
                     end = mid-1;
                 } else{
                     st = mid+1;
                 }
             } else{
-                if(target>=nums[mid]&&target<=nums[end]){
+                if(target>nums[mid]&&target<=nums[end]){
                     st = mid+1;
                 } else{
                     end = mid-1;
