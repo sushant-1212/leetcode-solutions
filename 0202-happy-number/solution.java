b97f0a4df60fd6cede816cff82e85614c0c0cb51
sushant-1212 <sushantferia11@gmail.com>
2026-01-24 13:34:43 +0000

    Sync LeetCode submission Runtime - 2 ms (14.39%), Memory - 42.7 MB (11.74%)

--- 0202-happy-number/solution.java
+++ 0202-happy-number/solution.java
@@ -1,28 +1,23 @@
-import java.util.*;
-
 class Solution {
     public boolean isHappy(int n) {
         Set<Integer> seen = new HashSet<>();
-
-        while (n != 1) {
-            if (seen.contains(n)) {
-                return false; 
+        while(n!=1){
+            if(seen.contains(n)){
+                return false;
+            } else{
+                seen.add(n);
+                n = getSum(n);
             }
-            seen.add(n);
-            n = getSum(n);
         }
         return true;
     }
-
-    private int getSum(int n) {
-        int sum = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            sum += digit * digit;
-            n /= 10;
+    private int getSum(int n){
+          int sum = 0;
+        while(n>0){
+            int digit = n%10;
+            sum+=digit*digit;
+            n=n/10;
         }
         return sum;
     }
 }
-
-
