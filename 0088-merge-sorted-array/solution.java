f0cbb37b18cb141af5deea012def7a1ef5ee77ac
sushant-1212 <sushantferia11@gmail.com>
2026-01-24 12:59:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (78.60%)

--- 0088-merge-sorted-array/solution.java
+++ 0088-merge-sorted-array/solution.java
@@ -14,10 +14,9 @@ class Solution {
             k--;
         }
         while(j>=0){
-            nums1[k]=nums2[j];
+            nums1[k] = nums2[j];
             j--;
             k--;
         }
-
     }
 }
