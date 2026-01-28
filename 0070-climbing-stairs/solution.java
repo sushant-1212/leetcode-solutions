2102f035c89c5bbd52ac5a9eef389e77ed9e3390
sushant-1212 <sushantferia11@gmail.com>
2026-01-28 06:30:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.1 MB (54.55%)

--- 0070-climbing-stairs/solution.java
+++ 0070-climbing-stairs/solution.java
@@ -1,26 +1,23 @@
 class Solution {
-    int[] dp;
-    public int climbStairs(int n) {
-        dp = new int[n+1];
-        for(int i = 0; i<=n; i++){
-            dp[i]=-1;
-        }
-        return recursion(n);
-    }
-    private int recursion(int n){
-        if(n==0){
+    int[]dp;
+    public int recursion(int n){
+        if(n==0 || n==1){
             return 1;
-        }
-        if(n<0){
-            return 0;
         }
         if(dp[n]!=-1){
             return dp[n];
         }
-        int opt1 = recursion(n-1);
-        int opt2 = recursion(n-2);
-        int ans = opt1 + opt2;
+        int ops1 = recursion(n-1);
+        int ops2 = recursion(n-2);
+        int ans = ops1+ops2;
         dp[n] = ans;
         return ans;
+    }
+    public int climbStairs(int n) {
+        dp = new int[46];
+        for(int i = 0; i<46; i++){
+            dp[i] = -1;
+        }
+        return recursion(n);
     }
 }
