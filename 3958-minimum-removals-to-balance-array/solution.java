129e47ef145a3520387555b94365abe9ecd4b4a1
sushant-1212 <sushantferia11@gmail.com>
2026-02-06 17:46:12 +0000

    Sync LeetCode submission Runtime - 25 ms (54.69%), Memory - 86.2 MB (45.57%)

---
+++ 3958-minimum-removals-to-balance-array/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int minRemoval(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        int l = 0;
+        int maxLen = 1;
+
+        for (int r = 0; r < n; r++) {
+            while ((long) nums[r] > (long) nums[l] * k) {
+                l++;
+            }
+            maxLen = Math.max(maxLen, r - l + 1);
+        }
+
+        return n - maxLen;
+    }
+}
+
