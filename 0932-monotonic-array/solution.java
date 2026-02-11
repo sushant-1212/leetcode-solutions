b0db23005bed63f0589d9ef123d7e69aec4a591a
sushant-1212 <sushantferia11@gmail.com>
2026-02-11 19:07:21 +0000

    Sync LeetCode submission Runtime - 2 ms (68.25%), Memory - 85.4 MB (7.60%)

---
+++ 0932-monotonic-array/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        int n = nums.length;
+        boolean inc = true;
+        boolean dec = true;
+        for(int i = 1; i<n; i++){
+            if(nums[i-1]<nums[i]){
+                dec = false;
+            }
+            if(nums[i-1]>nums[i]){
+                inc = false;
+            }
+        }
+        return inc || dec;
+    }
+}
