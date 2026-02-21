599240518246fcb5e727d1e09ed910bd4b1de223
sushant-1212 <sushantferia11@gmail.com>
2026-02-21 19:35:23 +0000

    Sync LeetCode submission Runtime - 1 ms (86.32%), Memory - 47.2 MB (74.73%)

--- 0547-number-of-provinces/solution.java
+++ 0547-number-of-provinces/solution.java
@@ -1,13 +1,14 @@
 class Solution {
-    int[]vis;
+    int[] vis;
     public void dfs(int src , int[][] isConnected){
         vis[src] = 1;
         for(int i = 0; i<isConnected[src].length; i++){
-            int val = isConnected[src][i];
-            if(val==1 && vis[i]==0){
+            int neighbour = isConnected[src][i];
+            if(neighbour == 1 && vis[i]==0){
                 dfs(i,isConnected);
             }
         }
+
     }
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
