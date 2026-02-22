7431d7c395090f73bf39d60b34a7d77880ffcdd6
sushant-1212 <sushantferia11@gmail.com>
2026-02-22 10:46:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.9 MB (24.65%)

--- 0733-flood-fill/solution.java
+++ 0733-flood-fill/solution.java
@@ -1,25 +1,33 @@
 class Solution {
-    public void dfs(int[][] image, int i, int j, int color , int srcC , int n , int m){
-        if(i<0 || j<0 || i>=n || j>=m){
+    public void dfs(int[][] image, int i, int j, int color , int src , int n , int m){
+        
+        if(i < 0 || j < 0 || i >= n || j >= m){
             return;
         }
-        if(image[i][j]!=srcC){
+        
+        if(image[i][j] != src){
             return;
         }
+        
         image[i][j] = color;
-        dfs(image,i-1,j,color,srcC,n,m);
-        dfs(image,i+1,j,color,srcC,n,m);
-        dfs(image,i,j-1,color,srcC,n,m);
-        dfs(image,i,j+1,color,srcC,n,m);
+        
+        dfs(image , i-1 , j , color, src , n , m);
+        dfs(image , i+1 , j , color, src , n , m);
+        dfs(image , i , j-1 , color, src , n , m);
+        dfs(image , i , j+1 , color, src , n , m);
     }
+    
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int srcC = image[sr][sc];
+        
         int n = image.length;
         int m = image[0].length;
-        if(srcC!=color){
-             dfs(image,sr,sc,color,srcC,n,m);
-        }
-        return image;
         
+        int src = image[sr][sc];
+        
+        if(src != color){
+            dfs(image, sr, sc, color, src, n, m);
+        }
+        
+        return image;
     }
 }
