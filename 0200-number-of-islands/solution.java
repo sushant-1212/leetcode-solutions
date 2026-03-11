7fd1b87d2b7f7c5ece811320fc3936668f7c8666
sushant-1212 <sushantferia11@gmail.com>
2026-03-11 19:21:41 +0000

    Sync LeetCode submission Runtime - 4 ms (46.41%), Memory - 52.1 MB (67.12%)

--- 0200-number-of-islands/solution.java
+++ 0200-number-of-islands/solution.java
@@ -1,16 +1,16 @@
 class Solution {
-    int n;
     int m;
+    int n;
     int[][]vis;
     char[][] mat;
     public void dfs(int i , int j){
-        if(i<0 || j<0 || i>=n || j>=m){
+        if(i<0 || j<0 || i>=m || j>=n){
             return;
         }
         if(mat[i][j]=='0'){
             return;
         }
-        if(vis[i][j]==1){
+        if(mat[i][j]=='1' && vis[i][j]==1){
             return;
         }
         vis[i][j] = 1;
@@ -18,19 +18,18 @@ class Solution {
         dfs(i-1,j);
         dfs(i,j+1);
         dfs(i,j-1);
-
     }
     public int numIslands(char[][] grid) {
         mat = grid;
-        n = grid.length;
-        m = grid[0].length;
-        vis = new int[n][m];
+        m = grid.length;
+        n = grid[0].length;
+        vis = new int[m][n];
         int count = 0;
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(vis[i][j]== 0 && mat[i][j]=='1'){
-                    count++;
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(grid[i][j]=='1' && vis[i][j]==0){
                     dfs(i,j);
+                    count++;
                 }
             }
         }
