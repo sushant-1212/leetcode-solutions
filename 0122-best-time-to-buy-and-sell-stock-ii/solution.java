383927b88c1fabb25bd0638d004a2434055a2c28
sushant-1212 <sushantferia11@gmail.com>
2026-03-26 18:59:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.6 MB (17.72%)

--- 0122-best-time-to-buy-and-sell-stock-ii/solution.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/solution.java
@@ -1,10 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int profit = 0;
+        int maxprofit = 0;
 
         for(int i = 1; i < prices.length; i++) {
             if(prices[i] > prices[i-1]) {
                 profit += prices[i] - prices[i-1];
+                maxprofit = Math.max(maxprofit,profit);
             }
         }
 
