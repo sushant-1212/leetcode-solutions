f64ed95bf5c966e2e08cd4c749863d7f74a72faa
sushant-1212 <sushantferia11@gmail.com>
2026-04-06 17:44:32 +0000

    Sync LeetCode submission Runtime - 18 ms (13.55%), Memory - 46.8 MB (9.08%)

---
+++ 0242-valid-anagram/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i = 0; i<s.length(); i++){
+            char c = s.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(int i = 0; i<t.length(); i++){
+            char c = t.charAt(i);
+            if(!map.containsKey(c)){
+                return false;
+            }
+            map.put(c,map.get(c)-1);
+            if(map.get(c)==0){
+                map.remove(c);
+            }
+        }
+        return map.isEmpty();
+    }
+}
