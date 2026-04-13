a3b52e3c6e89189aa4a949dcc8c4c1e18eeb844d
sushant-1212 <sushantferia11@gmail.com>
2026-04-13 10:09:06 +0000

    Sync LeetCode submission Runtime - 4 ms (46.41%), Memory - 51.9 MB (74.35%)

--- 0200-number-of-islands/solution.java
+++ 0200-number-of-islands/solution.java
@@ -1,23 +1,31 @@
 class Solution {
     int m;
     int n;
+    char[][]mat;
     int[][]vis;
-    char[][] mat;
     public void dfs(int i , int j){
         if(i<0 || j<0 || i>=m || j>=n){
+            return;
+        }
+        if(vis[i][j]==1 && mat[i][j]=='1'){
             return;
         }
         if(mat[i][j]=='0'){
             return;
         }
-        if(mat[i][j]=='1' && vis[i][j]==1){
-            return;
+        vis[i][j]=1;
+        if(i+1<m && j<n && vis[i+1][j]==0){
+            dfs(i+1,j);
         }
-        vis[i][j] = 1;
-        dfs(i+1,j);
-        dfs(i-1,j);
-        dfs(i,j+1);
-        dfs(i,j-1);
+         if(i-1>=0 && j<n && vis[i-1][j]==0){
+            dfs(i-1,j);
+        }
+         if(i<m && j+1<n && vis[i][j+1]==0){
+            dfs(i,j+1);
+        }
+         if(i<m && j-1>=0 && vis[i][j-1]==0){
+            dfs(i,j-1);
+        }
     }
     public int numIslands(char[][] grid) {
         mat = grid;
@@ -27,7 +35,7 @@ class Solution {
         int count = 0;
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                if(grid[i][j]=='1' && vis[i][j]==0){
+                if(mat[i][j]=='1' && vis[i][j]==0){
                     dfs(i,j);
                     count++;
                 }
