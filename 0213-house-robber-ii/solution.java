1398650c32c9df821461c8a78868abf7046d516b
sushant-1212 <sushantferia11@gmail.com>
2026-04-14 17:20:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.7 MB (77.86%)

---
+++ 0213-house-robber-ii/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if(n == 1) return nums[0];
+
+        int case1 = robHelper(nums, 0, n - 2);
+        int case2 = robHelper(nums, 1, n - 1);
+
+        return Math.max(case1, case2);
+    }
+
+    private int robHelper(int[] nums, int start, int end) {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for(int i = start; i <= end; i++) {
+            int pick = nums[i] + prev2;
+            int notPick = prev1;
+
+            int curr = Math.max(pick, notPick);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
