5a5762988378e9fdd8836c5a1e2f7d9ea408dc85
sushant-1212 <sushantferia11@gmail.com>
2026-04-26 17:17:23 +0000

    Sync LeetCode submission Runtime - 22 ms (31.09%), Memory - 136.5 MB (71.33%)

--- 1663-detect-cycles-in-2d-grid/solution.java
+++ 1663-detect-cycles-in-2d-grid/solution.java
@@ -1,26 +1,27 @@
 class Solution {
+    char[][] mat;
+    int[][]vis;
     int m;
     int n;
-    int[][] vis;
-    char[][] mat;
     boolean hasCycle;
-    public void dfs(int i , int j , int pi , int pj){
-        if(i<0 || j<0 || i>=m || j>=n){
-            return ;
+    public void dfs(int i, int j, int pi, int pj){
+        if(i<0 || i>=m || j<0 || j>=n){
+            return;
         }
-        vis[i][j] = 1;
+        vis[i][j]=1;
         if(i-1>=0 && mat[i-1][j]==mat[i][j]){
             if(vis[i-1][j]==0){
                 dfs(i-1,j,i,j);
-            } else if(!(i-1 == pi && j == pj)){
+            } else if(!(i-1==pi && j==pj)){
                 hasCycle = true;
                 return;
             }
         }
+
         if(i+1<m && mat[i+1][j]==mat[i][j]){
             if(vis[i+1][j]==0){
                 dfs(i+1,j,i,j);
-            } else if(!(i+1 == pi && j == pj)){
+            } else if(!(i+1==pi && j==pj)){
                 hasCycle = true;
                 return;
             }
@@ -28,7 +29,7 @@ class Solution {
         if(j-1>=0 && mat[i][j-1]==mat[i][j]){
             if(vis[i][j-1]==0){
                 dfs(i,j-1,i,j);
-            } else if(!(i == pi && j-1 == pj)){
+            } else if(!(i==pi && j-1==pj)){
                 hasCycle = true;
                 return;
             }
@@ -36,28 +37,29 @@ class Solution {
         if(j+1<n && mat[i][j+1]==mat[i][j]){
             if(vis[i][j+1]==0){
                 dfs(i,j+1,i,j);
-            } else if(!(i == pi && j+1 == pj)){
+            } else if(!(i==pi && j+1==pj)){
                 hasCycle = true;
                 return;
             }
         }
     }
+
     public boolean containsCycle(char[][] grid) {
         mat = grid;
         m = grid.length;
-        n = grid[0].length;
+         n = grid[0].length;
         vis = new int[m][n];
-        hasCycle = false;
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
                 if(vis[i][j]==0){
                     dfs(i,j,-1,-1);
                 }
-                if(hasCycle){
-                    return true;
-                }
             }
         }
+        if(hasCycle){
+            return true;
+        }
         return false;
+        
     }
 }
