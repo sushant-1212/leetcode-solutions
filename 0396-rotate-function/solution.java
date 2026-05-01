2d93abd87a5c5f9bf947a0585a1d6b8112f8e14e
sushant-1212 <sushantferia11@gmail.com>
2026-05-01 20:40:22 +0000

    Sync LeetCode submission Runtime - 4 ms (98.61%), Memory - 90.7 MB (97.92%)

---
+++ 0396-rotate-function/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+
+        int n = nums.length;
+
+        int sum = 0;
+        int f = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            f += i * nums[i];
+        }
+
+        int ans = f;
+
+        
+        for (int k = 1; k < n; k++) {
+            f = f + sum - n * nums[n - k];
+            ans = Math.max(ans, f);
+        }
+
+        return ans;
+    }
+}
