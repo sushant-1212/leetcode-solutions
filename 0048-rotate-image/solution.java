ad4c5910c3273523602c4cb30b149b34f2ca76bf
sushant-1212 <sushantferia11@gmail.com>
2026-05-04 13:22:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (70.89%)

--- 0048-rotate-image/solution.java
+++ 0048-rotate-image/solution.java
@@ -1,28 +1,23 @@
 class Solution {
-    public void rev(int row[]){
-        int i = 0;
-        int j = row.length-1;
-        while(i<j){
-            int temp = row[i];
-            row[i] = row[j];
-            row[j] = temp;
-            i++;
-            j--;
-        }
-    }
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                if(i<j){
-                    int temp = matrix[i][j];
-                    matrix[i][j] = matrix[j][i];
-                    matrix[j][i] = temp;
-                }
+        for(int i=0; i<matrix.length; i++){
+            for(int j=i; j<matrix.length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+
             }
         }
-        for(int i = 0; i<n; i++){
-            rev(matrix[i]);
+        for(int i=0; i<matrix.length; i++){
+            int left=0;
+            int right = matrix.length-1;
+            while(left<right){
+                int temp = matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+            }
         }
     }
 }
