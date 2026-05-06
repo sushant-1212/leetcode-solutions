bf7b8a408bf6577b0080102428d36123afe2d460
sushant-1212 <sushantferia11@gmail.com>
2026-05-06 20:45:07 +0000

    Sync LeetCode submission Runtime - 7 ms (90.49%), Memory - 124.8 MB (43.80%)

---
+++ 1972-rotating-the-box/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int m = boxGrid.length;
+        int n = boxGrid[0].length;
+        for (int i = 0; i < m; i++) {
+            int empty = n - 1;
+            for (int j = n - 1; j >= 0; j--) {
+                if (boxGrid[i][j] == '*') {
+                    empty = j - 1;
+                }
+                else if (boxGrid[i][j] == '#') {
+                    boxGrid[i][j] = '.';
+                    boxGrid[i][empty] = '#';
+                    empty--;
+                }
+            }
+        }
+        char[][] ans = new char[n][m];
+
+
+        for(int i = 0; i < m; i++) {
+
+
+            for(int j = 0; j < n; j++) {
+
+                ans[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+        return ans;
+    }
+}
