f2f4917f9fdc732ed58f7b4172f1a5bb22af9d2a
sushant-1212 <sushantferia11@gmail.com>
2026-05-14 15:43:31 +0000

    Sync LeetCode submission Runtime - 5 ms (44.55%), Memory - 45.1 MB (35.51%)

---
+++ 2892-check-if-array-is-good/solution.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isGood(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int n = nums[nums.length - 1];
+
+ 
+        if(nums.length != n + 1)
+            return false;
+
+     
+        for(int i = 0; i < n - 1; i++) {
+            if(nums[i] != i + 1)
+                return false;
+        }
+
+     
+        return nums[n - 1] == n && nums[n] == n;
+    }
+}
