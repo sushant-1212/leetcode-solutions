d6ed3ced87fcf57f0ed6ee522bf3dc6dfd38112c
sushant-1212 <sushantferia11@gmail.com>
2026-05-31 15:16:41 +0000

    Sync LeetCode submission Runtime - 26 ms (74.88%), Memory - 107.5 MB (50.29%)

---
+++ 2245-destroying-asteroids/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+
+        Arrays.sort(asteroids);
+
+        long currMass = mass;
+
+        for(int asteroid : asteroids) {
+
+            if(currMass < asteroid) {
+                return false;
+            }
+
+            currMass += asteroid;
+        }
+
+        return true;
+    }
+}
