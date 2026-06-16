01347c59af8b43de34814d9a914e5e834fd06864
sushant-1212 <sushantferia11@gmail.com>
2026-06-16 18:12:17 +0000

    Sync LeetCode submission Runtime - 3 ms (99.70%), Memory - 55.6 MB (11.77%)

---
+++ 3931-process-string-with-special-operations-i/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch >='a' && ch<= 'z'){
+                sb.append(ch);
+            }
+            else if(ch=='*'){
+                if(sb.length()>0){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+                
+            } else if(ch=='#'){
+                sb.append(sb.toString());
+            } else if(ch=='%'){
+                sb.reverse();
+            }
+        }
+        return sb.toString();
+        
+    }
+}
