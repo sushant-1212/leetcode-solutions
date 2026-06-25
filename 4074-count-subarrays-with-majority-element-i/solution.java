0e9f45fa01c2f6814421b204361a15e8774d96a9
sushant-1212 <sushantferia11@gmail.com>
2026-06-25 15:03:01 +0000

    Sync LeetCode submission Runtime - 50 ms (20.17%), Memory - 47.2 MB (5.64%)

--- 4074-count-subarrays-with-majority-element-i/solution.java
+++ 4074-count-subarrays-with-majority-element-i/solution.java
@@ -1,22 +1,27 @@
 class Solution {
     public int countMajoritySubarrays(int[] nums, int target) {
-        int count = 0;
+
+        int ans = 0;
         int n = nums.length;
 
-        for (int i = 0; i < n; i++) {
-            int freq = 0;
-            for (int j = i; j < n; j++) {
-                if (nums[j] == target) {
-                    freq++;
-                }
-                int length = j - i + 1;
-                if (freq > length / 2) {
+        for(int i = 0; i < n; i++) {
+
+            int count = 0;
+
+            for(int j = i; j < n; j++) {
+
+                if(nums[j] == target) {
                     count++;
+                }
+
+                int len = j - i + 1;
+
+                if(count > len / 2) {
+                    ans++;
                 }
             }
         }
 
-        return count;
+        return ans;
     }
 }
-
