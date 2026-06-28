3c33802169d5266771b9cad92ce14971f51f4659
sushant-1212 <sushantferia11@gmail.com>
2026-06-28 13:05:48 +0000

    Sync LeetCode submission Runtime - 9 ms (66.05%), Memory - 77.8 MB (16.05%)

--- 1956-maximum-element-after-decreasing-and-rearranging/solution.java
+++ 1956-maximum-element-after-decreasing-and-rearranging/solution.java
@@ -1,16 +1,19 @@
+import java.util.Arrays;
+
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+
         Arrays.sort(arr);
-        int n = arr.length;
-        int max = 0;
-        arr[0]=1;
-        max = arr[0];
-        for(int i=1; i<n; i++){
-            if(arr[i]>arr[i-1]+1){
-                arr[i] = arr[i-1]+1;
+
+        arr[0] = 1;
+
+        for(int i = 1; i < arr.length; i++){
+
+            if(arr[i] > arr[i-1] + 1){
+                arr[i] = arr[i-1] + 1;
             }
-            max = Math.max(max,arr[i]);
         }
-        return max;
+
+        return arr[arr.length - 1];
     }
 }
