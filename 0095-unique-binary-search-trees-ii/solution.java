49be42db06d63739e0e4ffbfea8668fc6b63b5b8
sushant-1212 <sushantferia11@gmail.com>
2026-07-14 17:47:51 +0000

    Sync LeetCode submission Runtime - 1 ms (99.29%), Memory - 46.6 MB (21.52%)

---
+++ 0095-unique-binary-search-trees-ii/solution.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+import java.util.*;
+
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<>();
+        return build(1, n);
+    }
+
+    private List<TreeNode> build(int start, int end) {
+        List<TreeNode> result = new ArrayList<>();
+
+        if (start > end) {
+            result.add(null); 
+            return result;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftSubtrees = build(start, i - 1);
+            List<TreeNode> rightSubtrees = build(i + 1, end);
+
+            for (TreeNode left : leftSubtrees) {
+                for (TreeNode right : rightSubtrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    result.add(root);
+                }
+            }
+        }
+
+        return result;
+    }
+}
