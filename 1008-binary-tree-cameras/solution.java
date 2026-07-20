c62bab5c77afbefccd6ac09281bca76becdf0cf5
sushant-1212 <sushantferia11@gmail.com>
2026-07-20 15:10:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.4 MB (87.61%)

---
+++ 1008-binary-tree-cameras/solution.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    int cameras = 0;
+
+    public int minCameraCover(TreeNode root) {
+
+        if (dfs(root) == 0)
+            cameras++;
+
+        return cameras;
+    }
+
+    private int dfs(TreeNode node) {
+
+        if (node == null)
+            return 2;
+
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+
+        if (left == 0 || right == 0) {
+            cameras++;
+            return 1;
+        }
+
+        if (left == 1 || right == 1)
+            return 2;
+
+        return 0;
+    }
+
+}
