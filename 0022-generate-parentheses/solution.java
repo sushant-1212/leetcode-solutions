7997f8a0de649459503ee29184426ed1995e35c6
sushant-1212 <sushantferia11@gmail.com>
2026-07-21 17:48:14 +0000

    Sync LeetCode submission Runtime - 2 ms (69.52%), Memory - 44.9 MB (20.88%)

--- 0022-generate-parentheses/solution.java
+++ 0022-generate-parentheses/solution.java
@@ -1,23 +1,26 @@
 class Solution {
 
-    public void helper(List<String> ans, String str, int open, int close, int n) {
+    List<String> ans = new ArrayList<>();
 
-        if(str.length() == 2 * n) {
-            ans.add(str);
+    public List<String> generateParenthesis(int n) {
+        backtrack("", 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(String s, int open, int close, int n) {
+
+        if (s.length() == 2 * n) {
+            ans.add(s);
             return;
         }
 
-        if(open < n) helper(ans, str + "(", open + 1, close, n);
+        if (open < n) {
+            backtrack(s + "(", open + 1, close, n);
+        }
 
-        if(close < open) helper(ans, str + ")", open, close + 1, n);
-    }
-
-    public List<String> generateParenthesis(int n) {
-
-        List<String> ans = new ArrayList<>();
-
-        helper(ans, "", 0, 0, n);
-
-        return ans;
+        if (close < open) {
+            backtrack(s + ")", open, close + 1, n);
+        }
     }
 }
+
