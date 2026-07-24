70575354ceb2b6bd38f73e409d9e2952a26ee269
sushant-1212 <sushantferia11@gmail.com>
2026-07-24 18:25:28 +0000

    Sync LeetCode submission Runtime - 3 ms (100.00%), Memory - 46.1 MB (18.91%)

--- 0201-bitwise-and-of-numbers-range/solution.java
+++ 0201-bitwise-and-of-numbers-range/solution.java
@@ -1,9 +1,13 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
-        while(left<right){
-            right = right & (right-1);
+        int shift = 0;
+
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            shift++;
         }
-        return right;
-        
+
+        return left << shift;
     }
 }
