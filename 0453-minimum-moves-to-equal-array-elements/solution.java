e00d461654e5ba01eabe956c4c56e23544384c35
sushant-1212 <sushantferia11@gmail.com>
2026-07-25 17:48:52 +0000

    Sync LeetCode submission Runtime - 2 ms (91.49%), Memory - 47.4 MB (20.14%)

---
+++ 0453-minimum-moves-to-equal-array-elements/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minMoves(int[] nums) {
+
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans += nums[i] - min;
+        }
+
+        return ans;
+    }
+}
