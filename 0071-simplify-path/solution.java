4ec4cd7e8a6d43d484b1f8f7e76d482ff19de5a7
sushant-1212 <sushantferia11@gmail.com>
2026-07-29 19:58:23 +0000

    Sync LeetCode submission Runtime - 5 ms (51.47%), Memory - 44.8 MB (62.86%)

---
+++ 0071-simplify-path/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+
+        for (String s : path.split("/")) {
+            if (s.equals("") || s.equals(".")) {
+                continue;
+            } else if (s.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(s);
+            }
+        }
+
+        if (stack.isEmpty()) return "/";
+
+        StringBuilder ans = new StringBuilder();
+        for (String s : stack) {
+            ans.append("/").append(s);
+        }
+
+        return ans.toString();
+    }
+}
