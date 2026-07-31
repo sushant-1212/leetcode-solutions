7da6048e90cd4bf7f62e05d59ba3056163ce636e
sushant-1212 <sushantferia11@gmail.com>
2026-07-31 06:29:19 +0000

    Sync LeetCode submission Runtime - 9 ms (94.22%), Memory - 47.9 MB (76.10%)

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+
+        for (int i = 0; i < word.length(); i++) {
+            freq[word.charAt(i) - 'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        int ans = 0;
+        int pos = 0;
+
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) break;
+
+            ans += freq[i] * (pos / 8 + 1);
+            pos++;
+        }
+
+        return ans;
+    }
+}
