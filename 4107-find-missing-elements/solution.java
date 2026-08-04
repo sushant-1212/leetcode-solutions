476e9e6e2074993c076db3a1b066e1d915850578
sushant-1212 <sushantferia11@gmail.com>
2026-08-04 17:19:09 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 46.9 MB (44.72%)

--- 4107-find-missing-elements/solution.java
+++ 4107-find-missing-elements/solution.java
@@ -2,38 +2,30 @@ import java.util.*;
 
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-        int n = nums.length;
+        int min = nums[0];
+        int max = nums[0];
 
-        // Step 1: Find min and max
-        int min = nums[0], max = nums[0];
-        for (int i = 1; i < n; i++) {
-            min = Math.min(min, nums[i]);
-            max = Math.max(max, nums[i]);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min) min = nums[i];
+            if (nums[i] > max) max = nums[i];
         }
 
-        // Step 2: Sort the array
-        Arrays.sort(nums);
+      
+        boolean[] present = new boolean[max + 1];
 
-        // Step 3: Binary search for each number between min and max
-        List<Integer> missing = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            present[nums[i]] = true;
+        }
+
+        
+        List<Integer> ans = new ArrayList<>();
+
         for (int i = min; i <= max; i++) {
-            if (!binarySearch(nums, i)) {
-                missing.add(i);
+            if (!present[i]) {
+                ans.add(i);
             }
         }
 
-        return missing;
-    }
-
-    // Custom binary search (returns true if target exists)
-    private boolean binarySearch(int[] arr, int target) {
-        int left = 0, right = arr.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] == target) return true;
-            else if (arr[mid] < target) left = mid + 1;
-            else right = mid - 1;
-        }
-        return false;
+        return ans;
     }
 }
