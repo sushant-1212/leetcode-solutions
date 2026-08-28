f08b1d4788f26eda54e9694c49c069231cdee6a6
sushant-1212 <sushantferia11@gmail.com>
2026-08-28 18:39:20 +0000

    Sync LeetCode submission Runtime - 11 ms (46.81%), Memory - 41.6 MB (96.71%)

---
+++ 0668-kth-smallest-number-in-multiplication-table/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findKthNumber(int m, int n, int k) {
+        int low = 0;
+
+        int high = m*n;
+
+        while(low<=high){
+
+            int mid = low + (high - low) / 2;
+
+            int count = 0;
+            
+            for(int i = 1; i<=m; i++){
+
+                count+=Math.min(n,mid/i);
+            }
+            if(count<k){
+                low = mid + 1;
+            } else{
+                high = mid-1;
+            }
+        }
+         return low;
+    }
+}
