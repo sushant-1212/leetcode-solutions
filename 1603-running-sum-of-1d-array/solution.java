a5b75aa1421b8f1a30606be8f82ff26235def678
sushant-1212 <sushantferia11@gmail.com>
2026-08-31 05:12:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.2 MB (71.99%)

--- 1603-running-sum-of-1d-array/solution.java
+++ 1603-running-sum-of-1d-array/solution.java
@@ -1,13 +1,9 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int n = nums.length;
-        int sum = 0;
-        int prefix[] = new int[n];
-        for(int i = 0; i<n; i++){
-            sum = sum + nums[i];
-            prefix[i] = sum;
+        for(int i=1; i<nums.length; i++){
+            nums[i] = nums[i] + nums[i-1];
         }
-        return prefix;
-     }
+        return nums;
+        
+    }
 }
- 
