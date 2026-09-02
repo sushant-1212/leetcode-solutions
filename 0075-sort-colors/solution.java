ee3991f5c0af6ebb6cc592416e550e5f8db6fd27
sushant-1212 <sushantferia11@gmail.com>
2026-09-02 19:02:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (22.97%)

--- 0075-sort-colors/solution.java
+++ 0075-sort-colors/solution.java
@@ -1,25 +1,25 @@
 class Solution {
-    private void swap(int[] nums , int i , int j){
+    public void swap(int[] nums, int i , int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
     public void sortColors(int[] nums) {
-        int n = nums.length;
         int low = 0;
         int mid = 0;
-        int high = n-1;
+        int high = nums.length - 1;
         while(mid<=high){
             if(nums[mid]==0){
                 swap(nums,low,mid);
                 low++;
                 mid++;
-            } else if(nums[mid]==1){
+            } else if(nums[mid] == 1){
                 mid++;
             } else{
                 swap(nums,mid,high);
                 high--;
             }
         }
+        
     }
 }
