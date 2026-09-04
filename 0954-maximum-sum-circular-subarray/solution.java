c4d1479d2fcfa91c6f4eaf6437f4ee81f83027b1
sushant-1212 <sushantferia11@gmail.com>
2026-09-04 20:24:21 +0000

    Sync LeetCode submission Runtime - 7 ms (50.82%), Memory - 50.8 MB (90.24%)

--- 0954-maximum-sum-circular-subarray/solution.java
+++ 0954-maximum-sum-circular-subarray/solution.java
@@ -3,19 +3,22 @@ class Solution {
         int total = nums[0];
         int currentSum = nums[0];
         int maxSum = nums[0];
-        int currentMin = nums[0];
-        int minSum = nums[0];
-        for(int i = 1; i<nums.length; i++){
+        int currMin = nums[0];
+        int finalMin = nums[0];
+
+        for(int i=1; i<nums.length; i++){
             total+=nums[i];
-            currentSum = Math.max(nums[i] , currentSum+nums[i]);
-            maxSum = Math.max(currentSum , maxSum);
-            currentMin = Math.min(nums[i] , currentMin+nums[i]);
-            minSum = Math.min(currentMin , minSum);
-            }
-             if(maxSum<0){
-                return maxSum;
+            currentSum = Math.max(nums[i],currentSum + nums[i]);
+            maxSum = Math.max(currentSum,maxSum);
+            currMin = Math.min(nums[i] , currMin+nums[i] );
+            finalMin = Math.min(currMin,finalMin);
+
         }
-        return Math.max(maxSum , total-minSum);
-        
+         if(maxSum<0){
+            return maxSum;
+         }
+         int circularSum = total-finalMin;
+         int ans = Math.max(maxSum,circularSum);
+         return ans;
     }
 }
