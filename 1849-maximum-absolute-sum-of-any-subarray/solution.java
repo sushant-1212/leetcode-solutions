8b7e9e61f86452ce0f31d0245b1a73a8478a0095
sushant-1212 <sushantferia11@gmail.com>
2026-09-04 20:54:27 +0000

    Sync LeetCode submission Runtime - 7 ms (56.24%), Memory - 66.6 MB (86.42%)

--- 1849-maximum-absolute-sum-of-any-subarray/solution.java
+++ 1849-maximum-absolute-sum-of-any-subarray/solution.java
@@ -3,14 +3,15 @@ class Solution {
         int currentSum = nums[0];
         int maxSum = nums[0];
         int currentMin = nums[0];
-        int maxMin = nums[0];
-        for(int i = 1; i<nums.length; i++){
-            currentSum = Math.max(nums[i] , currentSum + nums[i]);
-            maxSum = Math.max(currentSum , maxSum);
+        int finalMin = nums[0];
+        for(int i=1; i<nums.length; i++){
+            currentSum = Math.max(nums[i],currentSum + nums[i]);
+            maxSum = Math.max(currentSum,maxSum);
             currentMin = Math.min(nums[i] , currentMin + nums[i]);
-            maxMin = Math.min(currentMin , maxMin);
+            finalMin = Math.min(currentMin,finalMin);
         }
-        return Math.max(maxSum , Math.abs(maxMin));
+
+        return Math.max(maxSum,-finalMin);
         
     }
 }
