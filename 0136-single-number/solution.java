4a361ec7177de604b52f5e81bf66808b1e216de9
sushant-1212 <sushantferia11@gmail.com>
2026-09-08 07:33:07 +0000

    Sync LeetCode submission Runtime - 1 ms (99.94%), Memory - 46.8 MB (64.87%)

--- 0136-single-number/solution.java
+++ 0136-single-number/solution.java
@@ -1,10 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-        for(int i = 0; i<nums.length; i++){
-            ans = ans ^ nums[i];
+        int XOR = 0;
+        for(int i=0; i<nums.length; i++){
+            XOR^=nums[i];
         }
-        return ans;
-        
+        return XOR;
     }
 }
