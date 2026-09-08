ce7f1fd8b2ae47887923f6950cd2406146573cfa
sushant-1212 <sushantferia11@gmail.com>
2026-09-08 06:59:47 +0000

    Sync LeetCode submission Runtime - 4 ms (38.99%), Memory - 268.8 MB (17.49%)

--- 0189-rotate-array/solution.java
+++ 0189-rotate-array/solution.java
@@ -1,19 +1,19 @@
 class Solution {
-    private void rev(int[] nums , int l , int r){
-        while(l<r){
-            int temp=nums[l];
-            nums[l]=nums[r];
-            nums[r]=temp;
-            l++;
-            r--;
-        }
+public void rotateArray(int[] nums , int i , int j){
+    while(i<j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        i++;
+        j--;
     }
+}
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        k=k%n;
-        rev(nums,0,n-1);
-        rev(nums,0,k-1);
-        rev(nums,k,n-1);
+         k = k%n;
+         rotateArray(nums , 0 , n-1);
+         rotateArray(nums,0,k-1);
+         rotateArray(nums,k,n-1);
         
     }
 }
