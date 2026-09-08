fefd8b46800c714c7b0b60e3fd8cb5fb5fbeee9d
sushant-1212 <sushantferia11@gmail.com>
2026-09-08 05:26:00 +0000

    Sync LeetCode submission Runtime - 1 ms (99.85%), Memory - 69.4 MB (11.56%)

--- 0209-minimum-size-subarray-sum/solution.java
+++ 0209-minimum-size-subarray-sum/solution.java
@@ -6,13 +6,12 @@ class Solution {
         for(int right = 0; right<nums.length; right++){
             sum+=nums[right];
             while(sum>=target){
-                ans = Math.min(ans,right-left + 1);
-                sum-=nums[left];
+                ans = Math.min(ans,right-left+1);
+                sum -= nums[left];
                 left++;
             }
         }
-        if( ans == Integer.MAX_VALUE) return 0;
-
+        if(ans == Integer.MAX_VALUE) return 0;
         return ans;
         
     }
