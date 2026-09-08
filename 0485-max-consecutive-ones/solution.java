05789913fb30befe37d3844efd4eb41865c6857e
sushant-1212 <sushantferia11@gmail.com>
2026-09-08 07:27:23 +0000

    Sync LeetCode submission Runtime - 3 ms (58.98%), Memory - 52.5 MB (66.40%)

--- 0485-max-consecutive-ones/solution.java
+++ 0485-max-consecutive-ones/solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
+        int i = 0;
         int left = 0;
-        int ans = 0;
+        int ans = Integer.MIN_VALUE;
         for(int right = 0; right<nums.length; right++){
             if(nums[right] == 0){
-                left = right+1;
+                left = right + 1;
             }
             ans = Math.max(ans,right-left + 1);
         }
         return ans;
-        
     }
 }
