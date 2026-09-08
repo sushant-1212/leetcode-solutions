0abc608ccc092f5d645bae55f1bc625e585363f7
sushant-1212 <sushantferia11@gmail.com>
2026-09-08 07:11:00 +0000

    Sync LeetCode submission Runtime - 2 ms (91.78%), Memory - 47.4 MB (93.21%)

--- 0283-move-zeroes/solution.java
+++ 0283-move-zeroes/solution.java
@@ -1,17 +1,18 @@
 class Solution {
-    public void swap(int[] nums , int i, int j){
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-    }
-    
+public void swap(int[]nums , int i, int j){
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
     public void moveZeroes(int[] nums) {
         int i = 0;
-        for(int j = 0; j<nums.length; j++){
+        for(int j=0; j<nums.length; j++){
             if(nums[j]!=0){
                 swap(nums,i,j);
                 i++;
             }
+
         }
+        
     }
 }
