88d8a85bd66c545a93d947d476b5c5a8694e99af
sushant-1212 <sushantferia11@gmail.com>
2026-09-12 17:33:52 +0000

    Sync LeetCode submission Runtime - 17 ms (77.27%), Memory - 45.5 MB (66.73%)

---
+++ 0016-3sum-closest/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+
+        Arrays.sort(nums);
+
+        int ans = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (Math.abs(sum - target) < Math.abs(ans - target)) {
+                    ans = sum;
+                }
+
+                if (sum == target) {
+                    return sum;
+                }
+                else if (sum < target) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
