b80d50cb8dd51611595ebffd7ac4555e7addc28b
sushant-1212 <sushantferia11@gmail.com>
2026-09-14 20:04:38 +0000

    Sync LeetCode submission Runtime - 4 ms (98.32%), Memory - 47.8 MB (29.53%)

--- 0680-valid-palindrome-ii/solution.java
+++ 0680-valid-palindrome-ii/solution.java
@@ -1,24 +1,26 @@
 class Solution {
-    public boolean validPalindrome(String s) {
-        int l = 0;
-        int r = s.length()-1;
-        while(l<r){
-            if(s.charAt(l)==s.charAt(r)){
-                l++;
-                r--;
-            } else{
-                return isPalindrome(s,l+1,r) || isPalindrome(s,l,r-1);
+
+    public boolean check(String s, int i, int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
             }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return check(s,i+1,j) || check(s,i,j-1);
+            }
+            i++;
+            j--;
         }
         return true;
         
-    }
-    private boolean isPalindrome(String s , int l , int r){
-        while(l<r){
-            if(s.charAt(l)!=s.charAt(r)) return false;
-            l++;
-            r--;
-        }
-        return true;
     }
 }
