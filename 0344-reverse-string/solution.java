da1dc2ef8bec9d0d5a6e4ee46f138cf6d7415450
sushant-1212 <sushantferia11@gmail.com>
2026-09-14 16:55:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48.2 MB (73.77%)

--- 0344-reverse-string/solution.java
+++ 0344-reverse-string/solution.java
@@ -1,14 +1,16 @@
 class Solution {
-    public void reverseString(char[] s) {
-        int left = 0;
-        int right = s.length-1;
-        while(left<right){
-            char temp = s[left];
-            s[left] = s[right];
-            s[right]=temp;
-            left++;
-            right--;
+    public void swap(char[] s, int i, int j){
+        while(i<j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
         }
+
+    }
+    public void reverseString(char[] s) {
+        swap(s,0,s.length-1);
         
     }
 }
